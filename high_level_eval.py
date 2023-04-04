805f341e17007ed03f326be742435ef7fa4d50d4
cartercooper <carterjccooper@gmail.com>
2023-04-03 20:49:20 -0700

    commented

--- high_level_eval.py
+++ high_level_eval.py
@@ -6,13 +6,20 @@ import time
 
 def simple_moving_average(data, window_size):
 
+    # start tracking memory usage
     tracemalloc.start()
 
+    # calculate the simple moving average using a rolling window
     ma = data.rolling(window_size, center=True).mean()
+
+    # extract true values and predicted values for comparison
     y_true = data[window_size:]
     y_pred = ma[window_size-1:-1]
+
+    # calculate the root mean squared error RMSE between true and predicted values
     rmse = np.sqrt(((y_true - y_pred)**2).mean())
 
+    # get the peak memory usage and stop tracking memory usage
     peak_memory = tracemalloc.get_traced_memory()[1]
     tracemalloc.stop()
 
@@ -21,18 +28,24 @@ def simple_moving_average(data, window_size):
 
 def classical_time_series_decomposition(data, period, window_size):
 
+    # start tracking memory usage
     tracemalloc.start()
 
+    # calculate the seasonal component of the data
     seasonal_means = np.array([data[i::period].mean() for i in range(period)])
     seasonal_data = np.tile(seasonal_means, len(data) // period + 1)[:len(data)]
 
+    # calculate the trend component of the data
     trend_data = data - seasonal_data
     trend_rolling_avg = trend_data.rolling(window_size).mean()
 
+    # calculate the residual component of the data
     residual_data = trend_data - trend_rolling_avg
 
+    # compute the rmse between the original data and the sum of its decomposed components
     rmse = np.sqrt(((trend_rolling_avg[window_size-1:-1] + seasonal_data[window_size:] + residual_data[window_size:]) - data[window_size:]) ** 2).mean()
 
+    # get the peak memory usage and stop tracking memory usage
     peak_memory = tracemalloc.get_traced_memory()[1]
     tracemalloc.stop()
 
@@ -41,39 +54,49 @@ def classical_time_series_decomposition(data, period, window_size):
 
 def polynomial_regression(data, degree):
 
+    # start tracking memory usage
     tracemalloc.start()
 
+    # convert data to a dataframe if not already
     data = pd.DataFrame(data)
+
+    # get the dimensions of the data
     feature_size = data.shape[1]
     sample_size = data.shape[0]
-    
+
+    # calculate the number of terms in the polynomial expansion
     num_terms = int(np.power(degree + 1, feature_size - 1))
-    
+
+    # initialize x (feature matrix) and y (target vector)
     x = np.zeros((sample_size, num_terms))
     y = data.iloc[:, feature_size - 1].values
 
+    # compute the feature matrix x
     for i in range(sample_size):
         index = 0
         for comb in range(num_terms):
             temp_comb = comb
             value = 1
-            
+
             for feature in range(feature_size - 1):
                 power = temp_comb % (degree + 1)
                 value *= np.power(data.iloc[i, feature], power)
-                temp_comb //= (degree + 1)
-                
+                temp_comb = temp_comb // (degree + 1)
+
             x[i, index] = value
             index += 1
 
+    # perform polynomial regression by solving the normal equations
     x_transpose = x.T
     x_transpose_x = np.matmul(x_transpose, x)
     x_transpose_y = np.matmul(x_transpose, y)
 
     coefficients = np.linalg.solve(x_transpose_x, x_transpose_y)
 
+    # compute the rmse between the true values and the predicted values
     rmse = np.sqrt(np.mean(np.square(np.matmul(x, coefficients) - y)))
 
+    # get the peak memory usage and stop tracking memory usage
     peak_memory = tracemalloc.get_traced_memory()[1]
     tracemalloc.stop()
 
@@ -82,6 +105,10 @@ def polynomial_regression(data, degree):
 
 
 def main():
+
+    #load data
+    #each evaluation runs, calculating the time difference from before and after, RMSE, and peak memory usage
+    #all results printed
 
     turbine = pd.read_csv('turbine.csv', names=['generator', 'rotor', 'dir', 't-2', 't-1','real_power']).tail(150)
 
